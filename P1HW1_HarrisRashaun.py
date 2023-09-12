a18bb2a011f24c73e8abc0417d935ed724775fd5
RashaunHarris <143528925+RashaunHarris@users.noreply.github.com>
2023-09-12 11:46:54 -0400

    Add files via upload

---
+++ P1HW1_HarrisRashaun.py
@@ -0,0 +1,11 @@
+user_num = int(input('Enter integer:\n'))
+
+# Type your code here
+print('You entered:', user_num)
+user_num2 = user_num*user_num
+print(user_num, 'squared is', user_num2)
+user_num3 = user_num*user_num*user_num
+print('And', user_num, 'cubed is', user_num3, '!!')
+integer1 = int(input('Enter another integer:\n'))
+print(user_num,'+',integer1,'is',user_num + integer1)
+print(user_num,'*',integer1,'is',user_num * integer1)
